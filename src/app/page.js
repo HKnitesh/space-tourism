04c724cb8c4aed1c27fcda882d8d8d5c40d36f92
HKnitesh <niteshchaurasiya1910@gmail.com>
2023-07-27 10:45:39 +0530

    bg fix-01

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,14 @@
 
 export default function Home() {
   return (
-    <main
-      className="flex text-new_blue lg:bg-[url('/home/background-home-desktop.jpg')] md:bg-[url('/home/background-home-tablet.jpg')] bg-[url('/home/background-home-mobile.jpg')] bgi h-screen "
+    <main 
+      // className="flex text-new_blue lg:bg-[url('/home/background-home-desktop.jpg')] md:bg-[url('/home/background-home-tablet.jpg')] bg-[url('/home/background-home-mobile.jpg')] bgi h-screen "
+      className="flex bgi h-screen text-new_cream
+      lg:bg-[url('https://res.cloudinary.com/drw8eqw3i/image/upload/v1690431530/spaceturism_assets/home/background-home-desktop_hqft1w.jpg')] 
+      md:bg-[url('https://res.cloudinary.com/drw8eqw3i/image/upload/v1690431530/spaceturism_assets/home/background-home-tablet_lkydc3.jpg')] 
+      bg-[url('https://res.cloudinary.com/drw8eqw3i/image/upload/v1690431530/spaceturism_assets/home/background-home-mobile_grlms0.jpg')]  "
     >
-
+Hare krishna
     </main>
   )
 }
